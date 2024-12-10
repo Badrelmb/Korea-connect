b50c11188d56208a20986f42e02aa1e529d66eff
Badr <badr.elmalki.berrada@gmail.com>
2024-12-10 15:03:03 +0900

    sff

--- public/config/supabaseClient.js
+++ public/config/supabaseClient.js
@@ -1,14 +1,17 @@
-import { createClient } from "../node_modules/@supabase/supabase-js/dist/module/index.js";
-
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabaseUrl = "https://djwbuyirbqrduukiprqg.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqd2J1eWlyYnFyZHV1a2lwcnFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3Nzc5NTgsImV4cCI6MjA0ODM1Mzk1OH0.KUPmEXfXNCoS2ZRNnuBrZ-Y1YCR5tX5rk5BgVTguhiE";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqd2J1eWlyYnFyZHV1a2lwcnFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3Nzc5NTgsImV4cCI6MjA0ODM1Mzk1OH0.KUPmEXfXNCoS2ZRNnuBrZ-Y1YCR5tX5rk5BgVTguhiE";
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase URL or anon key. Check your .env file.');
 }
 
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
+
+
+
+
