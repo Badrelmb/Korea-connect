667d23b9c800dcd54cd08eabd134ae3b9e6dbe43
Badr <badr.elmalki.berrada@gmail.com>
2024-12-10 14:33:34 +0900

    hal

--- public/config/supabaseClient.js
+++ public/config/supabaseClient.js
@@ -1,8 +1,12 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.30.0/+esm';
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://djwbuyirbqrduukiprqg.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqd2J1eWlyYnFyZHV1a2lwcnFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3Nzc5NTgsImV4cCI6MjA0ODM1Mzk1OH0.KUPmEXfXNCoS2ZRNnuBrZ-Y1YCR5tX5rk5BgVTguhiE";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase URL or anon key. Check your .env file.');
+}
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
--- public/login.js
+++ public/login.js
@@ -1,8 +1,3 @@
-// Initialize Supabase client
-const supabase = supabase.createClient(
-  "https://djwbuyirbqrduukiprqg.supabase.co", // Replace with your Supabase Project URL
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqd2J1eWlyYnFyZHV1a2lwcnFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3Nzc5NTgsImV4cCI6MjA0ODM1Mzk1OH0.KUPmEXfXNCoS2ZRNnuBrZ-Y1YCR5tX5rk5BgVTguhiE" // Replace with your Supabase Anon Key
-);
 
 document
   .getElementById("login-form")
