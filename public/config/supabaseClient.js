f9cfbe3916338ecc7378167d89e155cca9a60756
Badr <badr.elmalki.berrada@gmail.com>
2024-12-10 14:56:21 +0900

    hej

--- public/config/supabaseClient.js
+++ public/config/supabaseClient.js
@@ -1,7 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "../node_modules/@supabase/supabase-js/dist/module/index.js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+const supabaseUrl = "https://djwbuyirbqrduukiprqg.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqd2J1eWlyYnFyZHV1a2lwcnFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3Nzc5NTgsImV4cCI6MjA0ODM1Mzk1OH0.KUPmEXfXNCoS2ZRNnuBrZ-Y1YCR5tX5rk5BgVTguhiE";
+
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase URL or anon key. Check your .env file.');
 }
